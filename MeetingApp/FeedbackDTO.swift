b4e2d18651889930fbb1b3f61c81763d80e28bf6
trolund@gmail.com <vxh77kdp>
2019-02-28 13:40:29 +0100

    DTO tilføjet

---
+++ MeetingApp/FeedbackDTO.swift
@@ -0,0 +1,16 @@
+//
+//  FeedbackDTO.swift
+//  MeetingApp
+//
+//  Created by Troels on 28/02/2019.
+//  Copyright © 2019 TJ. All rights reserved.
+//
+
+import Foundation
+
+class FeedbackDTO {
+
+    var feedback : Int?
+    var comment: String?
+    
+}
--- MeetingApp/feedbackViewController.swift
+++ MeetingApp/feedbackViewController.swift
@@ -12,13 +12,15 @@ class feedbackViewController: UIViewController {
     
     var vote: Int = 0
     
-    var Qustions: [String] = ["spørgsmål 1", "spørgsmål 2", "spørgsmål 3"]
+    var qustions: [String] = ["spørgsmål 1", "spørgsmål 2", "spørgsmål 3"]
     var index = 0
+    var answer: [FeedbackDTO] = []
     
     @IBOutlet weak var numberOfQustions: UILabel!
     @IBOutlet weak var TheQustion: UILabel!
     @IBOutlet weak var textFeedback: UITextField!
     @IBOutlet weak var progressline: UIView!
+    @IBOutlet weak var progressbar: UIView!
     
     @IBOutlet var btn: [UIButton]!
     
@@ -35,24 +37,36 @@ class feedbackViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        numberOfQustions.text = "\(index+1) af \(Qustions.count)"
-        TheQustion.text = Qustions[index]
+        numberOfQustions.text = "\(index+1) af \(qustions.count)"
+        TheQustion.text = qustions[index]
         // Do any additional setup after loading the view.
     }
     
     @IBAction func backbtn(_ sender: UIButton) {
         if(index > 0){
         index -= 1
-        numberOfQustions.text = "\(index+1) af \(Qustions.count)"
-        TheQustion.text = Qustions[index]
+        numberOfQustions.text = "\(index+1) af \(qustions.count)"
+        TheQustion.text = qustions[index]
+         //   progressline.frame.size.width = CGFloat(index/Qustions.count * Int(progressbar.frame.size.width))
+            // TODO unpack.
         }
     }
     @IBAction func nextbtn(_ sender: UIButton) {
-        if(index+1 < Qustions.count){
+        if(index+1 < qustions.count){
         index += 1
-        numberOfQustions.text = "\(index+1) af \(Qustions.count)"
-        TheQustion.text = Qustions[index]
+        numberOfQustions.text = "\(index+1) af \(qustions.count)"
+        TheQustion.text = qustions[index]
+        //progressline.frame.size.width = CGFloat(index/Qustions.count * Int(progressbar.frame.size.width))
+            packfeedback()
         }
+    }
+    
+    func packfeedback(){
+        let dto: FeedbackDTO = FeedbackDTO()
+        dto.comment = textFeedback.text
+        dto.feedback = vote
+        answer.append(dto)
+        print(answer)
     }
 
 }
